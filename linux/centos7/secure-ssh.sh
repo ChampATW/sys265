9ad4ebc5717cedaa7c52eb7d33755a0d94c1bd8f
root <root@docker1.andrew.local>
2019-02-26 16:09:22 -0500

    Bash script works

--- linux/centos7/secure-ssh.sh
+++ linux/centos7/secure-ssh.sh
@@ -1,4 +1,4 @@
-\#SECURE-SSH.sh
+#SECURE-SSH.sh
 #AUTHOR ChampATW
 #Creates a new ssh user called $1
 #Adds a public key to that users authorized keys file
@@ -7,8 +7,8 @@ echo "I hope this works!"
 sudo useradd sys265
 sudo mkdir -p  /home/sys265/ssh
 cd ~
-sudo cp /home/andrew/wordboi/linux/public-keys/id_rsa.pub /home/sys265/ssh/authorized_keys
-sudo chmod 700 /home/sys265/ssh
-sudo chmod 600 /home/sys265/ssh/authorized_keys
+sudo cp /home/andrew/wordboi/linux/public-keys/id_rsa.pub /home/sys265/.ssh/authorized_keys
+sudo chmod 700 /home/sys265/.ssh
+sudo chmod 600 /home/sys265/.ssh/authorized_keys
 sudo chown -R sys265:sys265 /home/sys265/.ssh
 echo "Did it work?"
